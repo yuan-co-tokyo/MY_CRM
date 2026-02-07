e08b7a64a4e550070db59648d8c8a440c138a8f1
yuan-co-tokyo <yuan.co.tokyo@gmail.com>
2026-02-07 12:15:32 +0900

    Add NestJS API scaffold

---
+++ packages/api/src/main.ts
@@ -0,0 +1,13 @@
+import "reflect-metadata";
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  const port = process.env.PORT ? Number(process.env.PORT) : 3000;
+  await app.listen(port);
+  // eslint-disable-next-line no-console
+  console.log(`API running on http://localhost:${port}`);
+}
+
+bootstrap();
