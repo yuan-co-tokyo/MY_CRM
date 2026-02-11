1381d8fa13afd56b322cdc37944dde6cbf804146
yuan-co-tokyo <yuan.co.tokyo@gmail.com>
2026-02-11 17:45:49 +0900

    Enable CORS for local frontend

--- packages/api/src/main.ts
+++ packages/api/src/main.ts
@@ -4,6 +4,10 @@ import { AppModule } from "./app.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: [/^http:\/\/localhost:\d+$/],
+    credentials: true
+  });
   const port = process.env.PORT ? Number(process.env.PORT) : 3000;
   await app.listen(port);
   // eslint-disable-next-line no-console
